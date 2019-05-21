207a35647b3e02ed7a91aff1f03b04451d6425fb
Herbert04 <37424458+Herbert04@users.noreply.github.com>
2019-05-21 17:26:11 -0300

    Questão 2
    Recursivo que imprima a média dos elementos de uma lista de inteiros e o número de elementos maiores do que a média.

---
+++ Media/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Program b = new Program();
+                List<int> lista_de_elementos = new List<int>();
+                lista_de_elementos.Add(8);
+                lista_de_elementos.Add(4);
+                lista_de_elementos.Add(10);
+                lista_de_elementos.Add(4);
+                lista_de_elementos.Add(10);
+                b.elemnto(lista_de_elementos);
+              
+                
+            }           
+        }
+
+       private Double mediaArt(List<int> lista_de_elementos, int tamanhoLista)
+        {
+            if (lista_de_elementos.Count == 0)
+            {
+                return 0;
+            }
+           
+            Double media = Convert.ToDouble(lista_de_elementos[0]) / tamanhoLista + mediaArt(lista_de_elementos.Skip(1).Take(lista_de_elementos.Count).ToList(), tamanhoLista);          
+            return media;          
+        }
+
+        private void elemnto(List<int> lista_de_elementos)
+        {
+            Double media = mediaArt(lista_de_elementos, lista_de_elementos.Count);
+            int total = 0;
+            foreach(int valor in lista_de_elementos)
+            {
+                if (Convert.ToDouble(valor) > media)
+                    total++;
+            }
+            Console.WriteLine("A media eh de:" + media + " e existe "+total+" elementos maiores que a media");
+        }
+    }
+}
---
+++ Recursivo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recursivo
+{
+    class Program
+    {
+        static void Main2(string[] args)
+        {
+            int[] arrayNumeros = { 3, 2, 1, 7, 8, 9 };          
+            inverterNumeros(arrayNumeros,0,arrayNumeros.Length-1);
+            
+            foreach(int i in arrayNumeros)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+
+        private static void inverterNumeros(int[] arrayNumeros, int v1, int v2)
+        {
+            if (v1 < v2) { 
+            trocarPosicao(arrayNumeros, v1, v2);
+            inverterNumeros(arrayNumeros, v1+1, v2-1);
+            }
+        }
+
+        private static void trocarPosicao(int[] arrayNumeros, int v1, int v2)
+        {
+            int aux = arrayNumeros[v1];
+            arrayNumeros[v1] = arrayNumeros[v2];
+            arrayNumeros[v2] = aux;
+        }
+    }
+    
+}
